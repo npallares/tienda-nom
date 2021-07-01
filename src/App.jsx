7f3a7793203901fe48d53c2199d349521c4c2d1c
Nicolas <nicolaspallaresdg@gmail.com>
2021-07-01 00:19:05 -0300

    BrowserRouter Andando

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Switch} from 'react-router-dom'
 import { Navbar } from './Components/navbarComponents/indexNavbar';
-import { Mercadolibre } from './Components/mlComponents/indexMl';
 import { ItemDetailContainer } from './Components/detailComponents/itemDetailContainer';
 import { ItemListContainer } from './Components/ItemComponents/itemListContainer';
 
@@ -11,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Navbar/>
       <Switch>
-        <Route exact path="/" component={Mercadolibre}/>
+        <Route exact path="/" component={ItemListContainer}/>
         <Route path="/producto/:productoId" component={ItemDetailContainer}/>
         <Route path="*" component={()=> <h1 className="red">404</h1>}/>
       </Switch>
--- src/Components/ItemComponents/itemListContainer.jsx
+++ src/Components/ItemComponents/itemListContainer.jsx
@@ -1,59 +1,47 @@
-import "./style.css"
+import "./style.css";
 import React, { useState, useEffect } from "react";
-import {Detail} from "../productComponents/indexProduct.jsx"
+import { Card } from "../ItemComponents/card";
 import { getData } from "../../util/getdata";
 
 export const ItemListContainer = () =>{
 
     const [items, setItems] = useState([]);
-    let contador = 0
-    
+    const [loading, setLoading] = useState(true);
+
     useEffect(()=>{
         document.title="Tienda Nom"
 
         const waitForData = async () =>{
-            let data = await getData("nike")
+            let data = await getData("adidas")
+            console.log(data)
             let aux = data.map(el=>{
                 return{
                     id: el.id,
                     title: el.title,
                     img: el.thumbnail,
-                    price: el.price,
-                    quantity: el.available_quantity
-                    
+                    price: el.price
                 }
-                
             });
             setItems(aux)
+            setLoading(false)
         }
-        
+
         waitForData()
-        
+
     },[])
     
-    
+    /* console.log(items) */
+
+    if(loading) return <h3>Loading...</h3>
+
     return(
-        <>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet"/> 
-            
-            <div className="container_item">
-
-                { 
-                
-                items.map(el=>{
-                    if( contador == 0 ){
-                        contador = 1;
-                        return <Detail img={el.img} title={el.title} description={el.price} cantidad={el.quantity}/>
-                    } 
-                    
-                })}
-
-                {/* ------- ACA -------- */}
-                {console.log(items[4])}
-                {/* {console.log(items[4].title)} */}
-               
-            </div>
-        </>
+        <div className="mlContainer">
+            {items.map(el=>{
+                return(
+                    <Card {...el}/>
+                )
+            })}
+        </div>
+        
     )
 }
