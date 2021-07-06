76bed7f418519773e65cbdf0ace9878d0642d04a
Nicolas <nicolaspallaresdg@gmail.com>
2021-07-05 21:10:14 -0300

    Entrega Sincronizar Counter / link

--- src/Components/ItemComponents/card.jsx
+++ src/Components/ItemComponents/card.jsx
@@ -1,10 +1,6 @@
 import { Link } from "react-router-dom"
 
-<<<<<<< HEAD
-export const Card = ({img,price,id}) =>{
-=======
 export const Card = ({img,id,price}) =>{
->>>>>>> prueba
     return(
         <>
         <div className="card">
